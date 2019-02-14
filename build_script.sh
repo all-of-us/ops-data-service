5109a50721a41b0852737699c140656b50f34626
michael.mead <michael.mead@vanderbilt.edu>
2019-02-14 13:22:47 -0600

    add .exe extension into build script

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
+        if [ $GOOS = "windows" ]; then
+            GOARCH+='.exe'
+        fi
         go build -v -o ops-data-$GOOS-$GOARCH
     done
 done
